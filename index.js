30e040093e71f8e7420ca37d352dab9139707830
ViktorCiobanu <ciobanu89@gmail.com>
2024-03-11 10:49:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const camelCase = 'camel case';
+const UPPER_SNAKE_CASE = 'upper snake case';
+const CapitaliseCase = 'capitalise case';
+
+const str = `acestea sunt cele trei variabile 1: ${camelCase} 2: ${UPPER_SNAKE_CASE} 3: ${CapitaliseCase}`;
+
+// console.log(str)
+
+// const num = 3;
+
+// const numToStr = num.toString();
+// const numToStr = String(num);
+// const numToStr = num + '';
+// const numToBooleann = Booleann(num);
+// const numBooleann = !!num;
+
+// console.log(numToBoolean)
+
+// const str1 = 'string';
+
+// const strToNumber = +str1;
+// const strToNNumber = Number(str1);
+
+// if(1) console.log('pass');
+// if(0) console.log('pass');
+// if(NaN) console.log('pass');
+// if(undefined) console.log('pass');
+// if(null) console.log('pass');
+// if(false) console.log('pass');
+
+// const ternary = 0 ? 'first option' : 'second option'
+
+// console.log(ternary)
+
+
+// if (0 || 1) console.log('or operator')
+// if (1 && 1) console.log('and operator')
+
+
+
+// const clientOrder = 'menu1'
+
+// switch (clientOrder) {
+//     case 'menu1' : 
+//         console.log('menu 1')
+//         break;
+//     case 'menu2' : 
+//         console.log('menu 2')
+//         break;
+//     case 'menu3' : 
+//         console.log('menu 3')
+//         break;
+//     case 'menu4' : 
+//         console.log('menu 4')
+//         break;
+//     default:
+//         console.log('nu avem asa conditie')
+// }
+
+
+
+
